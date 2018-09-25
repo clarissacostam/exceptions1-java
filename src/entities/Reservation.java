d27a5f87ba76e4fd9d68390290305945a27717f3
clarissa.martins <clarissa.martins@softplan.com.br>
2018-09-25 11:25:34 -0300

    Bad solution

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -43,16 +43,17 @@ public class Reservation {
 			return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // 
 		}
 		
-		public void updateDates(Date checkIn, Date checkOut) {
-/*			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				throw new DomainException("Reservation dates for update must be future dates");
+		public String updateDates(Date checkIn, Date checkOut) {
+			Date now = new Date();
+			if(checkIn.before(now) || checkOut.before(now)) {
+				return "Error in reservation: Reservation dates for update must be future dates";
 			}
 			if (!checkOut.after(checkIn)) {
-				throw new DomainException("Check-out date must be after check-in date");
-			}*/
+				return "Error in reservation: Check-out date must be after check-in date";
+			}
 			this.checkIn = checkIn;
 			this.checkOut = checkOut;
+			return null;
 		}
 		
 		@Override
